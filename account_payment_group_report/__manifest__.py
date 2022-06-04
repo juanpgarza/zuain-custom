1062832134c741de17f4a0ea22172e25f24e53fc
Juan Pablo Garza <juanp@juanpgarza.com>
2022-06-04 20:04:03 -0300

    [15.0][ADD] account_payment_group_report

---
+++ account_payment_group_report/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2022 juanpgarza - Juan Pablo Garza <juanp@juanpgarza.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "account payment group report ux",
+    "summary": "",
+    "version": "15.0.1.0.0",
+    "category": "Accounting",
+    "website": "https://github.com/juanpgarza/zuain-custom",
+    "author": "juanpgarza",
+    "license": "AGPL-3",
+    "depends": [
+        "account_payment_group",
+        "l10n_ar_ux",
+        ],
+    "data": [
+        'views/report_payment_group.xml',
+        ],
+    "installable": True,
+}
