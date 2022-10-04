e678f11750e6f3a26d5300e5d65ace573c31e26e
Juan Pablo Garza <juanp@juanpgarza.com>
2022-10-04 12:20:38 -0300

    [15.0][MIG] stock_picking_report_product_description

---
+++ stock_picking_report_product_description/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2021 openNova - Juan Pablo Garza <juanp@opennova.com.ar>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Stock picking report product description",    
+    "summary": "Change the product description in the stock reports",
+    "version": "13.0.1.0.0",
+    "category": "Stock",
+    "website": "https://github.com/OpenNovaSoft/misc-addons",
+    "author": "openNova",
+    "license": "AGPL-3",
+    "depends": ["stock"],
+    "data": [
+        'views/report_stockpicking_operations.xml',
+        ],
+    "development_status": "Production/Stable",        
+    "installable": True,
+}
