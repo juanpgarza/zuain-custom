cc5d884531779ad3270f9205d8b7c17f3e3b5313
Juan Pablo Garza <juanp@juanpgarza.com>
2023-03-18 18:14:02 -0300

    adaptaciones a actualizar_productos_desde_gap

--- actualizar_productos_desde_gap/wizards/update_price.py
+++ actualizar_productos_desde_gap/wizards/update_price.py
@@ -21,8 +21,11 @@ class ProductPricelistWizard(models.TransientModel):
             ProductPricelistWizard, self).default_get(field_names)
         # defaults['pricelist_id'] = self.env.context['active_id']
         return defaults
-    
+
     def do_update(self):
+
+        categorias = self.env['product.category'].search([])
+
         try:
             inputx = BytesIO()
             inputx.write(base64.decodestring(self.excel_file_for_import))
@@ -31,46 +34,59 @@ class ProductPricelistWizard(models.TransientModel):
             raise UserError(u'ERROR: {}'.format(e))
 
         sheet = book.sheets()[0]        
-        
-        modificaciones = 0
-        altas = 0
+
         for i in list(range(sheet.nrows)):
             if i == 0:
                 continue
             
             # 0
-            default_code = sheet.cell(i, 0).value
+            # art_id
+
             # 1
-            descripcion = sheet.cell(i, 1).value
-            # 2
-            replenishment_base_cost = sheet.cell(i, 2).value
-            
-            if default_code.strip() == "":
-                break            
+            default_code = sheet.cell(i, 1).value
+            # 2            
+            descripcion = sheet.cell(i, 2).value
+            # 3
+            replenishment_base_cost = sheet.cell(i, 3).value
+            # 4            
+            categ_id = self.get_category(default_code, categorias, sheet.cell(i, 4).value)
             
             # import pdb; pdb.set_trace()
-            product = self.env['product.template'].search([('default_code','=',default_code.strip())])
-            if product:
-                # actualizar precio
-                product.write({
-                    'replenishment_base_cost': replenishment_base_cost,
-                })
-                # 
-                _logger.info("{} actualizado ".format(default_code))
-                # import pdb; pdb.set_trace()
+
+            if type(default_code) is float:
+                default_code = str(int(default_code))
             else:
-                vals = {
-                    'default_code': default_code,
-                    'name': descripcion,
-                    'replenishment_base_cost': replenishment_base_cost,
-                    'standard_price': replenishment_base_cost,
-                }
-                self.env['product.template'].create(vals)
-                # import pdb; pdb.set_trace()
-                _logger.info("{} creado ".format(default_code))
-                # import pdb; pdb.set_trace()
-            if i % 100 == 0:
+                default_code = default_code.strip()
+
+            # import pdb; pdb.set_trace()
+
+            vals = {
+                'default_code': default_code,
+                'name': descripcion,
+                'replenishment_base_cost': replenishment_base_cost,
+                'standard_price': replenishment_base_cost,
+                'categ_id': categ_id,
+            }
+            self.env['product.template'].create(vals)
+
+            if i % 1000 == 0:
                 _logger.info("{} productos actualizados hasta ACA!".format(i))
+
+            # if i > 10:
+            #     break
         return
 
+
+    def get_category(self, default_code, categorias, nombre):
+        categ_id = categorias.filtered(lambda x: x.name == nombre)
+                
+        if categ_id:
+            categoria = categ_id.id
+        else:
+            # raise UserError("No existe la categoría: {} - codigo: {}".format(nombre,default_code))
+            _logger.info("No existe la categoría: {} - codigo: {}".format(nombre,default_code))
+            # le asigno "VARIOS"
+            categoria = 1
+
+        return categoria
     
